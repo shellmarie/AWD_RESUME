9b649d1dcca3f533dd8580693a503539c5d84ed3
shellmarie <155805233+shellmarie@users.noreply.github.com>
2024-04-03 00:05:32 +0800

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('colorButton').addEventListener('click', function() {
-    document.body.style.backgroundColor = '#FFE7C1';
-    document.body.style.color = '#555';
-    });
-    
